1fbed7a69c6cccd3adff9e5094b2e3204175ffe1
Alok Kumar Sahoo <aloksahoo9819@gmail.com>
2026-01-11 02:41:54 +0530

    fix: migration fixes

--- backend/migrations/000010_change_proxy_acl_unique_constraint.down.sql
+++ backend/migrations/000010_change_proxy_acl_unique_constraint.down.sql
@@ -1,7 +1,7 @@
 -- Revert unique constraint from (proxy_id, acl_group_id) back to (proxy_id, path_pattern)
 
 -- Drop the new unique constraint
-DROP INDEX IF EXISTS uq_proxy_acl_assignments_proxy_group;
+ALTER TABLE proxy_acl_assignments DROP CONSTRAINT IF EXISTS uq_proxy_acl_assignments_proxy_group;
 
 -- Recreate old unique constraint on (proxy_id, path_pattern)
-CREATE UNIQUE INDEX uq_proxy_acl_assignments_proxy_path ON proxy_acl_assignments(proxy_id, path_pattern);
+ALTER TABLE proxy_acl_assignments ADD CONSTRAINT uq_proxy_acl_assignments_proxy_path UNIQUE (proxy_id, path_pattern);
